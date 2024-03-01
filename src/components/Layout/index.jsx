0c7ae0ea8ef8b7f758012f1cdc19252c7e660ee4
Tal Kohavy <talkohavy@gmail.com>
2024-03-01 18:13:02 +0200

    used flex grow on main

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,15 +1,12 @@
 import Header from './Header/index';
 import Main from './Main/index';
-import Sidebar from './Sidebar/index';
 
 export default function Layout({ children }) {
   return (
-    <div className='h-full'>
+    <div className='flex h-full flex-col items-start justify-start'>
       <Header />
 
-      <div className='flex h-full items-center justify-center'>
-        <Sidebar />
-
+      <div className='flex w-full flex-grow items-center justify-center'>
         <Main>{children}</Main>
       </div>
     </div>
